0c05d27140c09edc3bb3a39764602cee26eb8aef
SenHayato <157812043+SenHayato@users.noreply.github.com>
2026-03-28 13:50:57 +0700

    Dialogue Update
    - Dialogue Image sistem update
    - Tambah Sprite di Project

--- Assets/Script/CarScript/CarDriver.cs
+++ Assets/Script/CarScript/CarDriver.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class CarDriver : MonoBehaviour
 {
-    [Header("Driver Sprite")]
+    [Header("Driver Image")]
     public Sprite driverDefault;
     public Sprite driverPanic;
     public Sprite driverMad;
 
     [Header("Dialogue")]
     public AudioClip[] voiceDefault;
+
+    public enum DriverReaction
+    {
+        Default, Panic, Mad
+    }
 }
--- Assets/Script/GameAdmin/DialogueScript.cs
+++ Assets/Script/GameAdmin/DialogueScript.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,7 +12,6 @@ public class DialogueScript : MonoBehaviour
     [Header("Dialogue Component")]
     [SerializeField] bool isActive = false;
     [SerializeField] Animation dialAnimation;
-    bool wasActive = false;
 
     [Header("Reference")]
     [SerializeField] CarDriver carDriver;
@@ -23,11 +23,46 @@ public class DialogueScript : MonoBehaviour
 
     void Start()
     {
-        
+
+    }
+
+    public void DialogueSetUp(CarDriver.DriverReaction driverReaction)
+    {
+        driverPicture.sprite = driverReaction switch
+        {
+            CarDriver.DriverReaction.Default => carDriver.driverDefault,
+            CarDriver.DriverReaction.Panic => carDriver.driverPanic,
+            CarDriver.DriverReaction.Mad => carDriver.driverMad,
+            _ => carDriver.driverDefault
+        };
+    }
+
+    IEnumerator DialogueEnable()
+    {
+        DialogueOn();
+
+        yield return new WaitForSeconds(1f);
+
+        DialogueOff();
+    }
+
+    void DialogueOn()
+    {
+        Debug.Log("Dial In");
+        isActive = true;
+    }
+
+    void DialogueOff()
+    {
+        Debug.Log("Dial Out");
+        isActive = false;
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            StartCoroutine(DialogueEnable());
+        }
     }
 }
